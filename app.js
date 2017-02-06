be2ae482475022e85508dcc85d62b02483a69db4
SkyAo <csvwolf@qq.com>
2017-02-06 14:05:07 +0800

    config initilize

---
+++ app.js
@@ -0,0 +1,3 @@
+import app from './src/index.vue'
+app.el = '#root'
+export default new Vue(app)
---
+++ src/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+      <image src="http://ata2-img.cn-hangzhou.img-pub.aliyun-inc.com/7d42775b8f0d0c86589082dc9ee179c2.png" class="bannar-image"/>
+      <text @click="changeMessage" class="message">{{ message }}</text>
+      <text class="quotes">{{ quotes }}</text>
+  </div>
+</template>
+
+<style>
+  .container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .bannar-image{
+    width:200;
+    height:200;
+  }
+
+  .message{
+    padding-top: 20;
+    padding-bottom: 40;
+    color: #0055dd;
+    font-size: 28; 
+  }
+  
+  .quotes{
+    font-size: 16;
+    color:#666; 
+  }
+</style>
+
+<script>
+  export default {
+    data() {
+      return {
+        message: 'Welcome to Use Weexpack!',
+        quotes: 'A Tool To Build WEEX Faster'
+      };
+    },
+    methods: {
+      changeMessage() {
+        this.message = 'You click it!';
+      }
+    }
+  }
+</script>
